a94272303a2743d9c9a07b7d9909006d3c15d7e3
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-05-24 21:57:22 +0300

    Authenticating page

---
+++ auth.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login/Signup</title>
+    <style>
+        .auth_page{
+            display: flex;
+            align-items: center;
+        }
+    </style>
+</head>
+<body>
+    <div class = "auth_page">
+        <form action="" method = "" name = "">
+            <label for="username">Username</label>
+            <input name = "username" id = "username" type = "text"><br>
+            <label for="password">Password</label>
+            <input name = "password" id = "password" type = "password"><br>
+            <input type="submit" id = "login" value="Login">
+            <input type="submit" id = "signup" value="Signup">
+        </form>
+        <div class = "filler">
+            <!-- <img = ""> -->
+        </div>
+    </div>
+    
+</body>
+</html>
