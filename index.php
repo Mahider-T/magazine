f0c6818f3def8ab95dce76a9cb529ef6c3ef5f23
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-05-24 11:40:53 +0300

    Add container for each blog heade

--- index.php
+++ index.php
@@ -33,5 +33,31 @@
             </ul>
         </div>
     </header>
+    <div class = "hero">
+        <!-- <img src = "hero.jpg"  height="500px"> -->
+        <div class = "button left-button"><a href= "#some_id"><img src = "left_circle_arrow.svg "></a></div>
+        <div class="hero-text">
+            <h2>What is this garbage?</h2><br>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing<br> 
+                elit. Eos, numquam iure. Possimus perspiciatis quas 
+                officiis.</p>
+        </div>
+        <div class = "button right-button"><a href= "#some_id"><img src = "right_circle_arrow.svg "></a></div>
+    </div>
+    <main>
+        <div class = "container">
+            <!-- <div class = "test" style="height: 500px; width: 500px; background-color:aqua"></div> -->
+            <div class = "divider divider_one">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Maiores nobis atque debitis rem asperiores eveniet provident 
+                    tempora eaque laudantium nisi voluptatibus assumenda incidunt 
+                    dolorem fuga, omnis sit dolor reiciendis expedita sapiente facilis accusamus error iusto odio. 
+                    Deserunt quod eaque sit iusto doloribus distinctio 
+                    laborum, accusamus natus esse, mollitia suscipit maxime.</p>
+            </div>
+            <div class = "divider divider_two"></div>
+            <div class = "divider divider_three"></div>
+        </div>
+    </main>
 </body>
 </html>
