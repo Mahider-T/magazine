83cb9529a4fa65462adef7dcd9200d3e2046ebb9
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-05-26 16:25:14 +0300

    Final authentication page

--- auth.php
+++ auth.php
@@ -6,25 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login/Signup</title>
     <style>
-        @import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@700&family=Nanum+Myeongjo&family=Shadows+Into+Light&display=swap');
+        /* @import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@700&family=Nanum+Myeongjo&family=Shadows+Into+Light&display=swap'); */
+        @import url('https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap');
         *{
             margin: 0;
             padding: 0;
         }
         .auth_page{
-            width: 40vw;
+            width: 50vw;
             height: 100vh;
             display: flex;
             flex-direction: column;
             justify-content: center;
             align-items: center;
             gap: 2rem;
-            border-right: 2px solid black;
+            padding-right: 10px;
+            flex-shrink: 1;
+            min-width: 550px;
 
         }
         .welcome{
             font-family: 'Times New Roman', Times, serif;
-            font-size: 40px;
+            font-family: 'Great Vibes', cursive;
+            margin-bottom: 30px;
+            font-size: 50px;
         }
         label{
             font-family: 'Times New Roman', Times, serif;
@@ -35,37 +40,46 @@
             display: flex;
         }
         #username,#password{
-            width: 300px;
-            height: 25px;
+            width: 400px;
+            height: 35px;
             margin-bottom: 10px;
         }
         #login, button{
-            width: 300px;
-            height: 40px;
+            width: 400px;
+            height: 45px;
             background-color: black;
             color: white;
             margin-top: 10px;
+            border-radius: 10px;
 
         }
         .low{
             display: flex;
-            width:300px;
+            width:400px;
             justify-content: space-between;
         }
         .header{
-            height: 50px;
-            width: 40vw;
-            background-color: lightgray;
-            margin-bottom: auto;
-            display: flex;
-            justify-content: flex-start;
-            align-items: center;
+           margin-bottom:auto;
+           margin-top: 50px;
         }
-        .footer{
-            height: 50px;
-            width: 40vw;
-            background-color: lightgray;
+        footer{
             margin-top: auto;
+            margin-top: auto;
+        }
+        .side_img{
+           object-fit:contain; 
+           flex-shrink: 2;
+        }
+        a{
+            text-decoration: none;
+            color: black;
+        }
+        .image{
+            display: flex;
+            min-width: 50vw;
+            height: 100vh;
+            align-items: center;
+            justify-content: center;
         }
 
         
@@ -75,33 +89,35 @@
     
     <div class = "auth_page">
         <div class="header">
-            <img src ="Icons/logo.svg" width="50px" height="50px">
+            <img src ="Icons/logo.svg" width="80px" height="80px">
         </div>
 
-        <div class = "welcome">Welcome back</div>
+        <div class = "welcome"><h1>Welcome back</h1></div>
         <div class = "form">
             <form action="" method = "" name = "">
                 <label for="username">Username</label><br>
-                <input name = "username" id = "username" type = "text"><br>
+                <input name = "username" id = "username" type = "text"><br><br>
                 <label for="password">Password</label><br>
-                <input name = "password" id = "password" type = "password"><br>
+                <input name = "password" id = "password" type = "password"><br><br>
+                <div class="low">
+                    <div class = "checkbox_and_label">
+                    <input type="checkbox" id = "remember">
+                    <label for = "remember">Remember me</label>
+            </div>
+                <a href="">Forgot Password?</a>
+            </div>
                 <input type="submit" id = "login" value="Login">
             </form>
         </div>
-        <div class="low">
-            <div class = "checkbox_and_label">
-                <input type="checkbox" id = "remember">
-                <label for = "remember">Remember me</label>
-            </div>
-            <a href="">Forgot Password?</a>
-        </div>
-        <button class = "google">Sign in with google</button>
-        <div class="footer">
+       
+        <footer>
+            <p>Don't have an account? <a href=""><strong>Sign up<strong</a></p>
+        </footer>
 
         </div>
     </div>
     <div class = "image">
-        <img src="Images/dt.png" width="100%" height="100%">
+        <img src="Images/image6.jpg" width="100%" height="100%"  class="side_image">
     </div>
     
 </body>
