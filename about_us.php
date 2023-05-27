c10a73d6c279b2a1097c2124efd6fcab835722a9
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-05-27 20:04:04 +0300

    Layout the general structure

--- about_us.php
+++ about_us.php
@@ -5,12 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php
         include('header.php');
     ?>
     <main class = "about_us">
+        <div class = "about_header"><h1>About us</h1></div>
+        <div class = "about1">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates libero dolorem consectetur laboriosam recusandae quasi voluptatum! Eum molestias aperiam ipsa rem adipisci distinctio, ipsum sapiente exercitationem quam voluptatem similique. Autem perferendis culpa error! Magni culpa asperiores provident nobis numquam quas.</p>
+        </div>
+        <div class = "about1 left"></div>
+        <div class = "about1 right"></div>
+        <div class = "about1 left"></div>
+        <div class = "about2 "></div>
+        <div class = "about2 "></div>
+        <div class = "about2 "></div>
+        <div class = "about1 left"></div>
+        <div class = "about1 right"></div>
+        <div class = "bottom_beauty">
+            <div class = "bb_child bb_child_left"></div>
+            <div class = "bb_child bb_child_right"></div>
+        </div>
+
+
+
+
     </main>
 
     <?php
