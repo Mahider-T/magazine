d07b4e30eab01c769d5aac300c909d83358196a5
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-05-27 19:11:41 +0300

    Create a header separate file shared among multiple pages

---
+++ footer.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<footer>
+        <div class = "title">
+            <p>Newspaper</p><hr> 
+            <p>Company</p><hr> 
+            <p>Categories</p>
+        </div><br>
+        <div class = "father_container">
+            <div class = "first footer_links">
+                Daily newspaper magazine with interesting 
+                    scientific articles and guides on modern technology 
+                    and fashion.
+            </div>
+            <div class = "second footer_links">
+                <ul>
+                    <li><a href = "">Home page</a></li>
+                    <li><a href = "">About us</a></li>
+                    <li><a href = "">Blog</a></li>
+                    <li><a href = "">Simple post</a></li>
+                    <li><a href = "">Shop</a></li>
+                </ul>
+            </div>
+            <div class = "third footer_links">
+                <ul>
+                    <li><a href = "">Home page</a></li>
+                    <li><a href = "">Privacy policy</a></li>
+                    <li><a href = "">Terms and conditions</a></li>
+                    <li><a href = "">Contact</a></li>
+                    <li><a href = "">Newslatter</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer> 
+    
+</body>
+</html>
