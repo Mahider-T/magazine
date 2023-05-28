fbfa88a6139108ad9b58b7e861fbbae6f2742e56
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-05-28 14:43:52 +0300

    Add sample text and photo

--- about_us.php
+++ about_us.php
@@ -11,26 +11,73 @@
     <?php
         include('header.php');
     ?>
+    <br><br><br>
     <main class = "about_us">
         <div class = "about_header"><h1>About us</h1></div>
-        <div class = "about1">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates libero dolorem consectetur laboriosam recusandae quasi voluptatum! Eum molestias aperiam ipsa rem adipisci distinctio, ipsum sapiente exercitationem quam voluptatem similique. Autem perferendis culpa error! Magni culpa asperiores provident nobis numquam quas.</p>
+        <div class = "about1 left first_row">
+            <img src = "Images/image2.jpg" width="100%" height="100%">
         </div>
-        <div class = "about1 left"></div>
-        <div class = "about1 right"></div>
-        <div class = "about1 left"></div>
-        <div class = "about2 "></div>
-        <div class = "about2 "></div>
-        <div class = "about2 "></div>
-        <div class = "about1 left"></div>
-        <div class = "about1 right"></div>
+        <div class = "about1 right first_row">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, sed. Perferendis unde accusantium porro hic dolores quam, reprehenderit maxime dignissimos dicta in pariatur atque culpa fugit neque provident assumenda Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi praesentium quasi esse ad vel, iste assumenda repellendus laborum aliquam molestias voluptatem, alias architecto officiis. Perferendis ratione laboriosam nobis vero fugiat!</p>
+        </div>
+        <div class = "about1 left second_row">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, sed. Perferendis unde accusantium porro hic dolores quam, reprehenderit maxime dignissimos dicta in pariatur atque culpa fugit neque provident assumenda Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi praesentium quasi esse ad vel, iste assumenda repellendus laborum aliquam molestias voluptatem, alias architecto officiis. Perferendis ratione laboriosam nobis vero fugiat</p>
+        </div>
+        <div class = "about1 right second_row">
+            <img src = "Images/image4.jpg" width="100%" height="100%">
+        </div>
+        <div class = "about2 ">
+            <h1> akdjfha ksdjfha</h1>
+            
+        </div>
+        <div class = "about2 ">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, doloribus ea omnis, deleniti minus, voluptatibus nobis sapiente voluptates saepe nulla odio nam accusamus quos explicabo corporis temporibus earum. Numquam, incidunt?</p>
+        </div>
+        <div class = "about2 ">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate, maxime eos? Sed doloribus dolor voluptatibus iste aspernatur autem quas veritatis asperiores omnis, consequuntur ex ratione modi perspiciatis maxime? Ex quia quasi inventore nisi labore, repellat asperiores iste odio porro iusto quibusdam praesentium consequatur aperiam eius? Ab, nam voluptate. Voluptatem, laudantium.</p>
+        </div>
+        <div class = "about1 left fourth_row">
+            <img src = "Images/image4.jpg" width="100%" height="100%">
+        </div>
+        <div class = "about1 right fourth_row">
+            <h1>01</h1>
+        </div>
         <div class = "bottom_beauty">
-            <div class = "bb_child bb_child_left"></div>
-            <div class = "bb_child bb_child_right"></div>
+            <div class = "bb_child bb_child_left">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis ad laudantium possimus nisi ipsam. Vitae temporibus dicta amet, magnam nihil aliquid labore consequuntur pariatur unde explicabo fuga veniam placeat esse?</p>
+            </div>
+            <div class = "bb_child bb_child_right">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus veniam, provident corrupti in magnam accusamus, necessitatibus mollitia iste beatae perspiciatis pariatur non possimus tempora fugit nostrum. Vel atque deleniti tempore?</p>
+            </div>
         </div>
 
-
-
+        <div class = "group_members">
+            <div class = "member_list">
+                <img src = "Images/dt.png" width="80%" height="50%" class="author">
+                <sub>Donald Trummp</sub>
+                <sub>Edtior</sub>
+            </div>
+            <div class = "member_list">
+                <img src = "Images/dt.png" width="80%" height="50%" class="author">
+                <sub>Donald Trummp</sub>
+                <sub>Edtior</sub>
+            </div>
+            <div class = "member_list">
+                <img src = "Images/dt.png" width="80%" height="50%" class="author">
+                <sub>Donald Trummp</sub>
+                <sub>Edtior</sub>
+            </div>
+            <div class = "member_list">
+                <img src = "Images/dt.png" width="80%" height="50%" class="author">
+                <sub>Donald Trummp</sub>
+                <sub>Edtior</sub>
+            </div>
+            <div class = "member_list">
+                <img src = "Images/dt.png" width="80%" height="50%" class="author">
+                <sub>Donald Trummp</sub>
+                <sub>Edtior</sub>
+            </div>
+        </div>
 
     </main>
 
