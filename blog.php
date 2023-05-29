7771a429980a075e0c1ed6fbb36d93456fcb8e6f
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-05-29 15:03:30 +0300

    Add a side navigation bar that show the list of already published blogs

---
+++ blog.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <link href="style.css" rel = "stylesheet">
+    <script src="script.js" defer></script>
+</head>
+<body>
+    <?php
+        include('header.php')
+    ?>
+    <aside id = "blog_aside">
+        <div id = "blog_aside_header">
+            <h1>Blogs</h1>
+        </div>
+        <div class = " blog_list blog1">
+            <h1>Title</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, facere. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, mollitia.</p>
+        </div>
+        <div class = " blog_list blog1">
+            <h1>Title</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, facere.</p>
+        </div>
+        <div class = " blog_list blog1">
+            <h1>Title</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, facere.</p>
+        </div>
+        <div class = " blog_list blog1">
+            <h1>Title</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, facere.</p>
+        </div>
+        <div class = " blog_list blog1">
+            <h1>Title</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, facere.</p>
+        </div>
+        <div class = " blog_list blog1">
+            <h1>Title</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, facere.</p>
+        </div>
+        <div class = " blog_list blog1">
+            <h1>Title</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, facere.</p>
+        </div>
+        <div class = " blog_list blog1">
+            <h1>Title</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, facere.</p>
+        </div>
+    </aside>
+
+</body>
+</html>
