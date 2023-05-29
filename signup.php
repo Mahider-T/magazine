dd36bace4b1ac7ea2935f913852abcf5f8db8b34
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-05-29 23:59:25 +0300

    Sign up page

---
+++ signup.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login/Signup</title>
+    <style>
+        /* @import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@700&family=Nanum+Myeongjo&family=Shadows+Into+Light&display=swap'); */
+        @import url('https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap');
+        *{
+            margin: 0;
+            padding: 0;
+        }
+        .auth_page{
+            display: flex;
+
+        }
+        .right{
+            display: flex;
+            min-width: 50vw;
+            height: 100vh;
+            border: 2px solid black;
+            box-sizing: border-box;
+            margin: 5px;
+            background-color: green;
+        }
+        /* .right img{
+            object-fit: contain;
+
+        } */
+        .left{
+            display: flex;
+            flex-direction: column;
+            /* align-items: center;
+            justify-content: center; */
+            min-width: 50vw;
+            height: 100vh;
+            border: 2px solid black;
+            box-sizing: border-box;
+            margin: 5px;
+        }
+        .left div{
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            gap:0px
+        }
+    </style>
+</head>
+<body>
+    
+    <div class = "auth_page">
+        <div class="left">
+            <div class = "icon"><img src ="Icons/logo.svg" width="80px" height="80px"></div>
+            <div class="header"><h1>Welcome</h1></div>
+            <div class = "form">
+                <form action="" method = "" name = "">
+                    <label for="username">Username</label><br>
+                    <input name = "username" id = "username" type = "text"><br><br>
+                    <label for="password">Password</label><br>
+                    <input name = "password" id = "password" type = "password"><br><br>
+                    <input type="submit" id = "login" value="Register">
+                </form>
+            </div>
+        </div>
+        <div class = "right">
+        <!-- <img src="Images/image6.jpg" width="100%" height="100%"  class="side_image"> -->
+    </div>
+    </div>
+    
+</body>
+</html>
