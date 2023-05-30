d39d0c7fd9b5e91d759e8791ab222054c7fbecca
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-05-30 13:47:55 +0300

    Skeleton of simple pos

---
+++ simple_post.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple post</title>
+    <link href="style.css" rel = "stylesheet">
+    <script src = "script.js" defer></script>
+</head>
+<body>
+    <?php
+        include('header.php')
+    ?>
+    <div id = "simple_post_container">
+        <main id = "simple_post_container_main">
+            <img src = "Images/image3.jpg" width="90%" height="50%">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus non iure nostrum ex hic beatae sequi atque aspernatur, laudantium ullam minus. Accusamus itaque maiores nobis doloremque, ipsum velit dolorum suscipit. Placeat tenetur unde soluta excepturi quibusdam deserunt provident ipsam dignissimos at corporis, iure ipsa non aliquam quam quisquam est cum sed voluptatibus minus sequi illo, totam ipsum maiores molestias! Explicabo dicta, ab autem quis ipsam quas beatae dolorem distinctio veritatis hic repudiandae et, totam aut eveniet quo labore quos nam soluta illum facilis! Beatae illo cumque, voluptatum sapiente eaque debitis iusto hic corrupti minima dolores voluptate consectetur eligendi libero laborum numquam porro eum illum veniam, iste perspiciatis itaque sit necessitatibus placeat. Dolor eligendi alias mollitia dolorum corrupti ipsam in consectetur nobis officiis, placeat autem nisi natus minus quia. Modi id neque, possimus ullam quisquam consectetur animi voluptatibus odio quaerat reiciendis mollitia ratione nemo dolorum unde quis nisi maiores eaque deleniti magni cumque quas laboriosam iste autem similique. Expedita iure non dolorem sapiente nesciunt, ut exercitationem quaerat! Modi blanditiis vero porro dignissimos, quasi iure saepe facere quidem quis, expedita officiis mollitia! Laudantium modi ab quasi aspernatur suscipit perferendis similique cum, at officia quos, nisi tempore non nemo excepturi sit assumenda iure maxime rerum vitae rem. Pariatur accusamus qui nobis? Iste magnam ratione assumenda libero. Ea laborum exercitationem sunt eaque aspernatur dicta et? Voluptate, corrupti impedit quos ratione ipsum at quas vero aliquid eum nulla mollitia accusantium delectus nihil expedita saepe pariatur, nesciunt adipisci nemo consectetur omnis. Autem assumenda aspernatur neque illum odio molestiae accusantium nostrum quibusdam in, reprehenderit excepturi praesentium nihil eveniet? Exercitationem, quibusdam? Nulla autem eaque incidunt adipisci minima similique dolor. Tenetur mollitia inventore error porro voluptatum vel aspernatur, harum, vitae architecto at delectus tempora, atque doloremque nam eligendi odit aliquid. Dicta facilis, repudiandae quia corporis in est maiores vitae incidunt temporibus natus aut praesentium ipsam laboriosam blanditiis laudantium nesciunt aperiam ullam consequuntur soluta! Quisquam, magnam cupiditate unde inventore, fuga harum suscipit ipsa, repellat incidunt odio tempore iste! Ipsa ratione laudantium quibusdam nulla nostrum aliquam sint eveniet id, dolorem officiis suscipit optio numquam placeat corrupti doloremque in fuga nisi fugit expedita! Incidunt, ullam assumenda illo odio accusantium dignissimos eaque molestias voluptatem voluptatibus, reprehenderit nostrum totam sunt, ut maxime hic magnam animi tempore magni aperiam! Sapiente doloribus esse iste porro quos quia! Ea consequuntur doloribus ratione delectus, minus similique, dolorem dolore possimus, eius molestias quam architecto fuga. Deleniti, deserunt cum?</p>
+        
+        </main>
+
+        <aside id = "simple_post_container_aside">
+            <div class="aside_list">
+                <h2>Title</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, laborum. Lorem ipsum dolor sit amet. Quasi, laborum. Lorem ipsum dolor sit amet.</p>
+                <button class = "simple_post_aside_button">Read more</button>
+            </div>
+            <div class="aside_list">
+            <h2>Title</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, laborum. Quasi, laborum. Lorem ipsum dolor sit amet. Quasi, laborum. Lorem ipsum dolor sit amet.</p>
+                <button class = "simple_post_aside_button">Read more</button>
+            </div>
+            <div class="aside_list last">
+                <h2>Title</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, laborum. Quasi, laborum. Lorem ipsum dolor sit amet. Quasi, laborum. Lorem ipsum dolor sit amet.</p>
+                <button class = "simple_post_aside_button">Read more</button>
+            </div>
+        </aside>
+    </div>
+    <?php
+        include('footer.php')
+    ?>
+</body>
+</html>
