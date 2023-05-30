47f7a7bab99f6e293a6f40e77799c7255468b012
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-05-30 20:27:01 +0300

    Change the heigh of aside container to auto

--- simple_post.php
+++ simple_post.php
@@ -9,36 +9,80 @@
     <script src = "script.js" defer></script>
 </head>
 <body>
-    <?php
-        include('header.php')
-    ?>
+    <div id = "header_in_simple_post">
+         <?php
+             include('header.php')
+         ?>
+    </div>
+   
     <div id = "simple_post_container">
         <main id = "simple_post_container_main">
-            <img src = "Images/image3.jpg" width="90%" height="50%">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus non iure nostrum ex hic beatae sequi atque aspernatur, laudantium ullam minus. Accusamus itaque maiores nobis doloremque, ipsum velit dolorum suscipit. Placeat tenetur unde soluta excepturi quibusdam deserunt provident ipsam dignissimos at corporis, iure ipsa non aliquam quam quisquam est cum sed voluptatibus minus sequi illo, totam ipsum maiores molestias! Explicabo dicta, ab autem quis ipsam quas beatae dolorem distinctio veritatis hic repudiandae et, totam aut eveniet quo labore quos nam soluta illum facilis! Beatae illo cumque, voluptatum sapiente eaque debitis iusto hic corrupti minima dolores voluptate consectetur eligendi libero laborum numquam porro eum illum veniam, iste perspiciatis itaque sit necessitatibus placeat. Dolor eligendi alias mollitia dolorum corrupti ipsam in consectetur nobis officiis, placeat autem nisi natus minus quia. Modi id neque, possimus ullam quisquam consectetur animi voluptatibus odio quaerat reiciendis mollitia ratione nemo dolorum unde quis nisi maiores eaque deleniti magni cumque quas laboriosam iste autem similique. Expedita iure non dolorem sapiente nesciunt, ut exercitationem quaerat! Modi blanditiis vero porro dignissimos, quasi iure saepe facere quidem quis, expedita officiis mollitia! Laudantium modi ab quasi aspernatur suscipit perferendis similique cum, at officia quos, nisi tempore non nemo excepturi sit assumenda iure maxime rerum vitae rem. Pariatur accusamus qui nobis? Iste magnam ratione assumenda libero. Ea laborum exercitationem sunt eaque aspernatur dicta et? Voluptate, corrupti impedit quos ratione ipsum at quas vero aliquid eum nulla mollitia accusantium delectus nihil expedita saepe pariatur, nesciunt adipisci nemo consectetur omnis. Autem assumenda aspernatur neque illum odio molestiae accusantium nostrum quibusdam in, reprehenderit excepturi praesentium nihil eveniet? Exercitationem, quibusdam? Nulla autem eaque incidunt adipisci minima similique dolor. Tenetur mollitia inventore error porro voluptatum vel aspernatur, harum, vitae architecto at delectus tempora, atque doloremque nam eligendi odit aliquid. Dicta facilis, repudiandae quia corporis in est maiores vitae incidunt temporibus natus aut praesentium ipsam laboriosam blanditiis laudantium nesciunt aperiam ullam consequuntur soluta! Quisquam, magnam cupiditate unde inventore, fuga harum suscipit ipsa, repellat incidunt odio tempore iste! Ipsa ratione laudantium quibusdam nulla nostrum aliquam sint eveniet id, dolorem officiis suscipit optio numquam placeat corrupti doloremque in fuga nisi fugit expedita! Incidunt, ullam assumenda illo odio accusantium dignissimos eaque molestias voluptatem voluptatibus, reprehenderit nostrum totam sunt, ut maxime hic magnam animi tempore magni aperiam! Sapiente doloribus esse iste porro quos quia! Ea consequuntur doloribus ratione delectus, minus similique, dolorem dolore possimus, eius molestias quam architecto fuga. Deleniti, deserunt cum?</p>
-        
+            <div id = "simple_post_image">
+                <sub>Dec 13, 2020</sub>
+                <img src= "Images/image4.jpg" width="100%" height="100%">
+            </div>
+            <div id = "simple_post_text">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores distinctio itaque sequi 
+                    cupiditate doloremque consequatur voluptatum similique aspernatur 
+                    praesentium dolores velit corrupti dolorem nulla laborum amet vel 
+                    tenetur odio omnis, molestias culpa deleniti soluta? Et voluptatibus 
+                    deserunt ad eos id, hic quos veritatis error nobis, laborum corporis, 
+                    delectus velit? Assumenda saepe dolore eius inventore a nam tempora quisquam, 
+                    dolorem magnam earum? </p>
+                    <p>Odit, unde hic sapiente minus consequatur repudiandae nostrum 
+                    expedita accusamus. Magnam cumque exercitationem, nobis voluptatibus blanditiis veritatis repudiandae 
+                    labore hic voluptates provident, nisi at impedit numquam suscipit molestiae placeat architecto ab aperiam 
+                    saepe facilis modi quos recusandae!</p> 
+                    <p>Beatae optio labore dicta, explicabo numquam soluta et? Quae, reiciendis
+                    est optio fuga temporibus hic deserunt eos nisi reprehenderit illum quam laborum qui aperiam. Omnis asperiores 
+                    non recusandae aut iste nobis voluptas. Placeat, qui, omnis ut reiciendis aliquid numquam repellat eveniet accusantium
+                    distinctio sequi dolorem. Obcaecati deserunt necessitatibus quisquam asperiores numquam nobis voluptates cupiditate voluptatibus, 
+                    tempora nulla quam doloremque explicabo quae suscipit sed inventore laborum repellat exercitationem quod praesentium dolor cum similique 
+                    ipsam perferendis! Voluptatum aut illo autem nobis praesentium, rem labore ea amet reprehenderit numquam ratione culpa tenetur provident 
+                    deserunt nam temporibus molestias? Exercitationem esse modi qui iusto?</p> 
+                    <p>Ea tempore cum delectus blanditiis similique accusantium fuga ratione deleniti sapiente nostrum, 
+                    magnam ullam sequi voluptatibus voluptate pariatur, iste cupiditate dicta repellendus esse molestias tempora natus ut odio quam! Velit at consectetur officia veritatis 
+                    dolor quam optio, voluptate distinctio perspiciatis voluptates eum laudantium libero assumenda nemo illum necessitatibus suscipit beatae. Iste porro quas mollitia, vitae
+                    soluta explicabo inventore deserunt eaque velit pariatur magnam assumenda voluptas quos, corporis quo et, ex expedita aliquam voluptatem atque! Maiores explicabo quaerat 
+                    officiis ipsum ullam assumenda, suscipit nihil totam odit tempora dignissimos hic sit nemo officia illo facere ipsam dicta? Ducimus laborum neque debitis dignissimos rem sed sit 
+                    explicabo sapiente inventore ad enim eum commodi facere ut nisi, harum ex at,</p>
+                    <p>quia corporis autem accusamus id officiis vel. Optio dolorum veniam ratione molestias atque non, sequi assumenda magni possimus? 
+                    Ipsa deserunt facilis maxime voluptates blanditiis, temporibus dolorem aperiam quidem officiis esse iusto, quia hic odit voluptatum minima dolor fugiat dignissimos? Corporis facilis cumque quas mollitia magni 
+                    at natus placeat itaque accusantium dolore voluptatum reiciendis, neque velit expedita quia quos tempora. Eveniet suscipit unde, enim soluta veniam rerum et? Ex excepturi consectetur ducimus quod corrupti quia ipsa veniam, 
+                    sed dolorem impedit incidunt tenetur rerum blanditiis. Deleniti eius impedit vero.</p>
+            </div>
+            
         </main>
 
         <aside id = "simple_post_container_aside">
             <div class="aside_list">
-                <h2>Title</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, laborum. Lorem ipsum dolor sit amet. Quasi, laborum. Lorem ipsum dolor sit amet.</p>
-                <button class = "simple_post_aside_button">Read more</button>
+                <h3>Title</h3><br>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga magnam quod praesentium enim 
+                    a aut exercitationem id sapiente sit voluptatum.</p>
+                <button class="aside_list_button">More</button>
             </div>
+
             <div class="aside_list">
-            <h2>Title</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, laborum. Quasi, laborum. Lorem ipsum dolor sit amet. Quasi, laborum. Lorem ipsum dolor sit amet.</p>
-                <button class = "simple_post_aside_button">Read more</button>
+            <h3>Title</h3><br>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga magnam quod praesentium enim 
+                    a aut exercitationem id sapiente sit voluptatum.</p>
+                <button class="aside_list_button">More</button>
             </div>
-            <div class="aside_list last">
-                <h2>Title</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, laborum. Quasi, laborum. Lorem ipsum dolor sit amet. Quasi, laborum. Lorem ipsum dolor sit amet.</p>
-                <button class = "simple_post_aside_button">Read more</button>
+
+            <div class="aside_list last">   
+            <h3>Title</h3><br>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga magnam quod praesentium enim 
+                    a aut exercitationem id sapiente sit voluptatum.</p>
+                <button class="aside_list_button">More</button> 
             </div>
+
         </aside>
     </div>
-    <?php
-        include('footer.php')
-    ?>
+    <div id = "footer_in_simple_post">
+        <?php
+            include('footer.php')
+        ?>
+    </div>
+    
 </body>
 </html>
