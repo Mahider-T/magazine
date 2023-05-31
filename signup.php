b261598c2cd2e76cee4bbc5f6976d22f018add24
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-05-31 12:56:40 +0300

    Add image and restructure left side

--- signup.php
+++ signup.php
@@ -49,6 +49,42 @@
         .left .icon{
             margin-bottom: -50px;
         }
+        #division_one{
+            display: flex;
+            flex-direction: column;
+            gap: 100px;
+
+
+        }
+        #division_two{
+            margin-top: -100px;
+        }
+        #division_two label{
+            font-size: x-large;
+            font-family: cursive;
+
+        }
+        #division_two .box{
+            width: 400px;
+            height: 30px;
+        }
+        #division_two #login{
+            width: 400px;
+            height: 45px;
+            background-color: black;
+            color: white;
+            margin-top: 10px;
+            border-radius: 10px;
+        }
+        #division_three{
+            height: 15%;
+            width: 100%;
+            background-color: lightgray;
+        }
+        #division_one h1{
+            font-family:cursive;
+        }
+
     </style>
 </head>
 <body>
@@ -58,27 +94,29 @@
     
     <div class = "auth_page">
         <div class="left">
+            <div id = "division_one">
+                <div class = "icon"><img src ="Icons/logo.svg" width="80px" height="80px"></div>
+                <div class="header"><h1>Welcome</h1></div>
+            </div>
 
-
-            <div class = "icon"><img src ="Icons/logo.svg" width="80px" height="80px"></div>
-            <div class="header"><h1>Welcome</h1></div>
-
-
-            <div class = "form">
+            <div id = "division_two">
                 <form action="" method = "" name = "">
                     <label for="username">Username</label><br>
-                    <input name = "username" id = "username" type = "text"><br><br>
+                    <input name = "username" id = "username" type = "text" class="box"><br><br>
                     <label for="password">Password</label><br>
-                    <input name = "password" id = "password" type = "password"><br><br>
-                    <label for="repear">Repeat password</label><br>
-                    <input name = "repeat" id = "repeat" type = "password"><br><br>
+                    <input name = "password" id = "password" type = "password" class="box"><br><br>
+                    <label for="repeat">Repeat password</label><br>
+                    <input name = "repeat" id = "repeat" type = "password" class="box"><br><br>
                     <input type="submit" id = "login" value="Register">
                 </form>
             </div>
+            <div id = "division_three">
+
+            </div>
         </div>
         <div class = "right">
-        <!-- <img src="Images/image6.jpg" width="100%" height="100%"  class="side_image"> -->
-    </div>
+            <img src="Images/image6.jpg" width="100%" height="100%"  class="side_image">
+        </div>
     </div>
     
 </body>
