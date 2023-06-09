1fc3ec99d6913e8b247ed0de37097ae13f62a4b4
Shan3774 <sifmek2@gmail.com>
2023-06-10 02:41:36 +0300

    adding the image input to the blog page

--- blog.php
+++ blog.php
@@ -41,26 +41,25 @@
     </aside>
     <main>
         <div id = "content_area">
-            <form id = "form" action="blog.php" method="post" >
+            <form id = "form" action="blog.php" method="post" enctype="multipart/form-data">
                 <label for = "post_field"> <h1>Write post here<h1></label>
+
                 <input class="inputs" id="author_name" name="author_name" type="text"placeholder="Enter author's name here." required></input>
-                <!-- <img  src="" alt=""> -->
+                
+                <label for="image">Upload Image</label>
+                <input type="file" name="image" accept="image/*" title="insert an image related to your blog"/>
+
                 <input class="inputs" type="text" name="title" id="title"  placeholder="Enter blog title here." required>
+                
                 <textarea id = "body" name="body" placeholder="Enter the blog content here." required></textarea><br><br>
+                
                 <input type = "submit" value="Post" id = "submit"><br><br>
+                
                 <input type = "file" id = "upload_file">
             </form>
+            <!--php form handling-->
             <?php
         if ($_SERVER['REQUEST_METHOD'] === 'POST'){//check if form is submitted
-            // Retrieve form data
-                //getting the publish date
-                $mydate=getdate(date("U"));
-                $publish_date = "$mydate[year]-$mydate[mon]-$mydate[mday]";
-                //getting other form inputs
-                $author_name = $_POST['author_name'];
-                $title = $_POST['title'];
-                $body = $_POST['body'];
-
                 //connect to the database
                 $host = "localhost";
                 $user = "root";
@@ -78,9 +77,27 @@
                     //     echo"<div id=\"submit_replyE\">Username is already taken</div>";
                     // }
                     // else{
+                
+                // Retrieve form data
+                $author_name = $_POST['author_name'];
+                $title = $_POST['title'];
+                $body = $_POST['body'];
+                $image = $_FILES["image"];
+                $info = getimagesize($image["tmp_name"]);
+                // check if $image is an image file
+                if(!$info)
+                {
+                    die("File is not an image");
+                }
+                $name = $image["name"];
+                $type = $image["type"];
+                $blob = addslashes(file_get_contents($image["tmp_name"]));
+
                 //Insert data into the database
-                $sql = "INSERT INTO blogs (authorname, publishdate, title, body)
-                VALUES ('$author_name', '$publish_date','$title', '$body')";
+                $sql = "INSERT INTO blogs (authorname, `image`, `name`, `type`, title, body)
+                VALUES ('$author_name','$blob','$name', '$type', '$title', '$body')";
+                mysqli_query($connection, $sql) or die(mysqli_error($connection));
+
                 //check if insertion was a success 
                 if ($connection->query($sql) === TRUE) {
                     echo "<script>
@@ -89,7 +106,10 @@
                     content_area.innerHTML = 'Blog posted successfully!';
                     </script>
                     <a href='index.php'>back to to home</a>";
-                }   
+                }
+                // else{
+                //     echo "<script>alert(\"couldn't record your data. please try again!\")</script>";
+                // }
         }
         ?>
         </div>
@@ -98,7 +118,6 @@
 <?php
     include("footer.php");
 ?>
-
 
 </body>
 </html>
